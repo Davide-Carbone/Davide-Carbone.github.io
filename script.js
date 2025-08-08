a7888cdce77df9260d86b7987033bfc1c0723ecb
Davide-Carbone <davidedaca@yahoo.it>
2025-08-08 16:24:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 barba.init({
+  sync: true,
   transitions: [{
     name: 'slide',
     async leave(data) {
-      // Animate current page sliding out to the left
       await gsap.to(data.current.container, {
         x: '-100%',
         opacity: 0,
@@ -11,15 +11,13 @@ barba.init({
       });
     },
     enter(data) {
-      // Set new page initially off-screen right
-      gsap.from(data.next.container, {
+      return gsap.from(data.next.container, {
         x: '100%',
         opacity: 0,
-        duration: 0.6,
+        duration: 1.2,      // slower fade-in + slide-in
         ease: 'power2.inOut'
       });
     },
-    // Optional: beforeEnter to reset scroll
     beforeEnter() {
       window.scrollTo(0, 0);
     }
