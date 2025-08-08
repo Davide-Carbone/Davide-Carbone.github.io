7ad9f0cc2b05b092afec180222e1485866698a8a
Davide-Carbone <davidedaca@yahoo.it>
2025-08-08 16:47:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 barba.init({
   transitions: [{
     name: 'slide',
-    leave(data) {
-      return gsap.to(data.current.container, {
+    leave({current}) {
+      return gsap.to(current.container, {
         x: '-100%',
         opacity: 0,
         duration: 0.6,
-        ease: "power1.inOut"
+        ease: 'power1.inOut'
       });
     },
-    enter(data) {
-      gsap.set(data.next.container, {
-        x: '100%',
-        opacity: 1
-      });
-      return gsap.to(data.next.container, {
+    enter({next}) {
+      gsap.set(next.container, {x: '100%', opacity: 1});
+      return gsap.to(next.container, {
         x: '0%',
         duration: 0.6,
-        ease: "power1.inOut"
+        ease: 'power1.inOut'
       });
     },
     beforeEnter() {
-      window.scrollTo(0, 0);  // reset scroll to top on new page
+      window.scrollTo(0, 0); // reset scroll to top on new page
     }
   }]
 });
