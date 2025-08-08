99e705f58f74d714226944453f5a945742f28e54
Davide-Carbone <davidedaca@yahoo.it>
2025-08-08 16:21:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-// Smooth page transitions with Barba.js and GSAP
-
 barba.init({
   transitions: [{
-    name: 'fade',
-    leave(data) {
-      return gsap.to(data.current.container, {
+    name: 'slide',
+    async leave(data) {
+      // Animate current page sliding out to the left
+      await gsap.to(data.current.container, {
+        x: '-100%',
         opacity: 0,
-        duration: 0.5
+        duration: 0.6,
+        ease: 'power2.inOut'
       });
     },
     enter(data) {
-      return gsap.from(data.next.container, {
+      // Set new page initially off-screen right
+      gsap.from(data.next.container, {
+        x: '100%',
         opacity: 0,
-        duration: 0.5
+        duration: 0.6,
+        ease: 'power2.inOut'
       });
+    },
+    // Optional: beforeEnter to reset scroll
+    beforeEnter() {
+      window.scrollTo(0, 0);
     }
   }]
 });
